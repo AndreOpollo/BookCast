4cde9e28794023c2e15bb84d78c04527c87a11b9
Andre <opolloandres@gmail.com>
2025-09-22 12:29:30 +0300

    [update] updates genre list screen layout

--- feature/genres/src/main/java/com/opollo/genres/GenreList.kt
+++ feature/genres/src/main/java/com/opollo/genres/GenreList.kt
@@ -1,17 +1,23 @@
 package com.opollo.genres
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.opollo.domain.model.Book
 import com.opollo.ui.BookCard
 
 @Composable
 fun GenreList(books:List<Book>){
-    LazyColumn(modifier = Modifier.fillMaxSize()){
-        items(books){
+    LazyColumn(modifier = Modifier.fillMaxSize(),
+        contentPadding = PaddingValues(16.dp),
+        verticalArrangement = Arrangement.spacedBy(12.dp)){
+        items(books,
+            key = {book->book.id}){
             book->
             BookCard(
                 book = book,
